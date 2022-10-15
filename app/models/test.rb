75cdec91f07ba409538453a038565c14c39fa451
Croud9 <slavokugfk@gmail.com>
2022-10-15 21:18:45 +0300

    Scope & validation

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,9 +4,17 @@ class Test < ApplicationRecord
   has_many :tests_users, :questions
   has_many :users, through: :tests_users
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :test_by_category, -> (category) { joins(:category).where('category.title = ?', category) }
+
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+  validates :level, numericality { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.title_test_by_category(category)
-    Test.joins('JOIN categories ON categories.id = tests.category_id')
-          .where(categories: { title: category }).order(title: :desc).pluck(:title)
+    test_by_category(category).order(title: :desc).pluck(:title)
   end
 end
  
